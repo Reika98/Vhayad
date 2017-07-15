ae46d6474d99a9ca19d6aad2bc4ef4642dd11af8
rdatibula <riidatibula@gmail.com>
2017-07-16 02:10:54 +0800

    edited some templates

--- vahay/admin.py
+++ vahay/admin.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Vahay, Image, Transaction
+from .models import Vahay, Image, Transaction, Resident
 
 # Register your models here.
 admin.site.register(Vahay)
 admin.site.register(Image)
 admin.site.register(Transaction)
+admin.site.register(Resident)
