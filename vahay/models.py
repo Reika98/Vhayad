857ca9486658bd6a8bdb1bc5e1fffa7a4c353305
rdatibula <riidatibula@gmail.com>
2017-07-15 19:47:47 +0800

    added some models

--- vahay/models.py
+++ vahay/models.py
@@ -9,9 +9,9 @@ from django.contrib.auth.models import User
 class Vahay(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE)
 	name = models.CharField(max_length=255)
-	rent_range = models.CharField(max_length=100)
 	category = models.CharField(max_length=100)
 	contact_details = models.CharField(max_length=100)
+	rent_range = models.CharField(max_length=100)
 	vote = models.IntegerField(default=0)
 	available = models.BooleanField(default=1)
 	description = models.CharField(max_length=500)
--- vahay/views.py
+++ vahay/views.py
@@ -9,11 +9,28 @@ import json
 # Create your views here.
 
 def vahay_details(request, pk):
-
 	if not request.user.is_authenticated:
 		return redirect('/')
 
 	return render(request, 'vahay/vahayDetails.html')
+
+
+def add_vahay(request):
+	if not request.user.is_authenticated:
+		return redirect('/')
+
+	if request.method == 'POST':
+		name = request.POST['name']
+		category = request.POST['category']
+		contact_details = request.POST['contact_details']
+		rent_range = request.POST['rent_range']
+		address = request.POST['address']
+		description = request.POST['description']
+		Vahay.objects.create(owner=request.user, name=name, rent_range=rent_range, category=category,
+			contact_details=contact_details, address=address, description=details)
+		return redirect('/')
+
+	return render(request, 'vahay/addVahay.html')
 
 
 def get_list_vahay(request):
