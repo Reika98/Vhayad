383593d35fce0cdd06876b4ce54e4f5b49c655fc
rdatibula <riidatibula@gmail.com>
2017-07-15 17:10:13 +0800

    edited sign in

--- vhayad/views.py
+++ vhayad/views.py
@@ -7,18 +7,19 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def home(request):
+	context = {}
+
 	if request.user.is_authenticated:
-		return render(request, 'vhayad/homepage.html')
+		return render(request, 'vhayad/homepage.html', context=context)
 
-	request.method == "POST":
-		username = request.POST['username']
-		password = request.POST['password']
-
+	if request.method == 'POST':
+		user = request.POST['username']
+		password =  request.POST['password']
 		user = authenticate(username=username, password=password)
-		
+
 		if user:
 			login(request, user)
-			return render(request, 'homepage.html', context=context)
+			return render(request, 'vhayad/homepage.html', context=context)
 		else:
 			context['error_message'] = 'Invalid username or password'
 			context['username'] = username
