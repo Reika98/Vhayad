6fa4158a434e3cf2dd8c83a22bd29eb8c27c3bb8
Reika98 <sjisantillan@gmail.com>
2017-07-15 15:21:39 +0800

    bank.py

--- payment/bank.py
+++ payment/bank.py
@@ -1,19 +1,22 @@
+from django.shortcuts import render, reverse, redirect
 import http.client
 
 conn = http.client.HTTPSConnection("api-uat.unionbankph.com")
 
-payload = "{\"channel_id\":\"UHAC_TEAM\",\"transaction_id\":\"001\",\"source_account\":\"XXXXXXXXXX1\",\"source_currency\":\"PHP\",\"target_account\":\"XXXXXXXXXX2\",\"target_currency\":\"PHP\",\"amount\":7}"
+def pay(request):
 
-headers = {
-    'x-ibm-client-id': "ad4dd380-baf8-4182-8b39-dd27bfee0999",
-    'x-ibm-client-secret': "I6gQ7qE6uN5fM6wV0kT3vA3tY2hB6kR4aI6cD1gV6pX1jS1mT3",
-    'content-type': "application/json",
-    'accept': "application/json"
-    }
+	payload = "{\"channel_id\":\"VHAYAD\",\"transaction_id\":\"002\",\"source_account\":\"101828352677\",\"source_currency\":\"PHP\",\"target_account\":\"101366553205\",\"target_currency\":\"PHP\",\"amount\":888"
 
-conn.request("POST", "/uhac/sandbox/transfers/initiate", payload, headers)
+	headers = {
+	    'x-ibm-client-id': "ad4dd380-baf8-4182-8b39-dd27bfee0999",
+	    'x-ibm-client-secret': "I6gQ7qE6uN5fM6wV0kT3vA3tY2hB6kR4aI6cD1gV6pX1jS1mT3",
+	    'content-type': "application/json",
+	    'accept': "application/json"
+	    }
 
-res = conn.getresponse()
-data = res.read()
+	conn.request("POST", "/uhac/sandbox/transfers/initiate", payload, headers)
 
-print(data.decode("utf-8"))
+	res = conn.getresponse()
+	data = res.read()
+
+	return data.decode("utf-8")
