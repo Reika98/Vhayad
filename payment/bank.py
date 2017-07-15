317fcf394ab971894229d1f13eb345d6b03e2aee
Reika98 <sjisantillan@gmail.com>
2017-07-15 23:20:09 +0800

    welpss

--- payment/bank.py
+++ payment/bank.py
@@ -15,27 +15,28 @@ def pay(request):
 	# source_account = info['resident_account']
 	# amount = info['amount_due']
 
-	account_num = vahay.models.Vahay.objects.filter(name='rafelle')
-	print account_num
+	query = vahay.models.Vahay.objects.filter(name='rafelle')
+	recipient = [ obj.main_as_json() for obj in list_obj ]
+	print recipient['account_num']
 
-	payload = "{\"channel_id\":\"VHAYAD\",\"transaction_id\":\"004\",\"source_account\":\"101828352677\",\"source_currency\":\"PHP\",\"target_account\":"+account_num+",\"target_currency\":\"PHP\",\"amount\":10}"
+	# payload = "{\"channel_id\":\"VHAYAD\",\"transaction_id\":\"003\",\"source_account\":\"101828352677\",\"source_currency\":\"PHP\",\"target_account\":\"101366553205\",\"target_currency\":\"PHP\",\"amount\":10}"
 
-	headers = {
+	# headers = {
 
-	    'x-ibm-client-id': "ad4dd380-baf8-4182-8b39-dd27bfee0999",
-	    'x-ibm-client-secret': "I6gQ7qE6uN5fM6wV0kT3vA3tY2hB6kR4aI6cD1gV6pX1jS1mT3",
-	    'content-type': "application/json",
-	    'accept': "application/json"  
+	#     'x-ibm-client-id': "ad4dd380-baf8-4182-8b39-dd27bfee0999",
+	#     'x-ibm-client-secret': "I6gQ7qE6uN5fM6wV0kT3vA3tY2hB6kR4aI6cD1gV6pX1jS1mT3",
+	#     'content-type': "application/json",
+	#     'accept': "application/json"  
 
-	}
+	# }
 
-	conn.request("POST", "/uhac/sandbox/transfers/initiate", payload, headers)
+	# conn.request("POST", "/uhac/sandbox/transfers/initiate", payload, headers)
 
-	res = conn.getresponse()
-	data = res.read()
+	# res = conn.getresponse()
+	# data = res.read()
 
-	print(data.decode("utf-8"))
+	# print(data.decode("utf-8"))
 
 	sample = { 'json': 'waaaaa'}
 
-	return HttpResponse(json.dumps(data), content_type="application/json")
+	return HttpResponse(json.dumps(sample), content_type="application/json")
--- vahay/urls.py
+++ vahay/urls.py
@@ -5,6 +5,7 @@ app_name = 'vahay'
 
 urlpatterns = [
     url(r'^vahay-details/(?P<pk>\d+)/$', views.vahay_details, name='vahay_details'),
+    url(r'^vahay-details-mobile/(?P<pk>\d+)/$', views.vahay_details, name='vahay_details'),
     url(r'^add-vahay/$', views.add_vahay, name='add_vahay'),
     url(r'^vahay-list/$', views.get_list_vahay, name='vahay_list'),
 ]
